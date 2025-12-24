c3a90188eb15f9075ec34b574b40e3bc88527cff
Aleksei Petrov <peta0982@gmail.com>
2025-12-24 20:18:45 +0500

    добавление истории в чате (в памяти)

--- src/main/java/ru/petrov/odata_bridge/service/AIService.java
+++ src/main/java/ru/petrov/odata_bridge/service/AIService.java
@@ -1,6 +1,8 @@
 package ru.petrov.odata_bridge.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.stereotype.Service;
 import ru.petrov.odata_bridge.config.AiConfig;
 
@@ -9,10 +11,12 @@ public class AIService {
     private final ChatClient chatClient;
     private final AiConfig aiConfig;
     private final IndexingService indexingService;
+    private final ChatMemory chatMemory;
 
-    public AIService(ChatClient.Builder chatClientBuilder, ODataService oDataService, AiConfig aiConfig, IndexingService indexingService) {
+    public AIService(ChatClient.Builder chatClientBuilder, ODataService oDataService, AiConfig aiConfig, IndexingService indexingService, ODataService oDataService1, ChatMemory chatMemory) {
         this.aiConfig = aiConfig;
         this.indexingService = indexingService;
+        this.chatMemory = chatMemory;
         this.chatClient = chatClientBuilder
                 .defaultTools(oDataService)  //Подключение инструментов с аннотацией Tool
                 .build();
@@ -35,12 +39,18 @@ public class AIService {
     public String getSmartResponse(String userPrompt) {
         // 1. RAG: Получаем технический контекст из векторов
         String context = indexingService.findRelevantMetadata(userPrompt);
+        // Используем константный ID для тестов, в будущем заменим на ID пользователя
+        String sessionId = "user-123";
 
         // 2. Orchestration: Формируем итоговый запрос к LLM
         return chatClient.prompt()
                 .system(s -> s.text(aiConfig.systemPrompt())
                         .param("context", context)) // Динамически внедряем контекст
                 .user(userPrompt)
+                .advisors(MessageChatMemoryAdvisor.builder(chatMemory)
+                        .conversationId(sessionId)
+                        .build())
+                .advisors(new org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor())
                 .call()
                 .content();
     }
